f62abe642b4aa95bc1f4f1be5330c2218d62e6e6
David Moulton <dbmoulton@gmail.com>
2023-02-12 19:23:15 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+const employee = {
+    name : "David",
+    streetAddress : "393 street"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = { ...employee };
+  
+    newObj[key] = value;
+  
+    return newObj;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+  
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee) {
+    const newObj = { ...employee };
+  
+    delete newObj.name;
+  
+    return newObj;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee) {
+    delete employee.name;
+  
+    return employee;
+};
